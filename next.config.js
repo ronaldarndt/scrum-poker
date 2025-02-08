e19b0f74cd450cdec30c2455060e834b1eb6d48a
ronaldarndt <ronaldarndtf@gmail.com>
2025-02-08 19:51:37 -0300

    remove jotai swc plugins

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  experimental: {
-    swcPlugins: [
-      ["@swc-jotai/react-refresh", {}],
-      ["@swc-jotai/debug-label", {}]
-    ]
-  }
 };
 
 module.exports = nextConfig;
